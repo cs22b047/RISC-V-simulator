ef95d39cb7b2eedb45d5cb6616596cb5d3603e3d
cs22b047 <cs22b047@iittp.ac.in>
2024-02-21 03:41:15 +0530

    created sim.cpp

--- sim.cpp
+++ sim.cpp
@@ -1,12 +1,21 @@
-class CPU{
-    private:
+#include <iostream>
+#include <string>
+using namespace std;
+class Core{
+    public:
         long int regs[32];
         long int pc;
+        string program[];
+    
 };
 class processor{
+    public:
+        int* memory = (int*)malloc(4096);
+        int clock =0;
+        Core cores[2] = {Core(),Core()};
+
 
 };
 int main(){
-    
-}
 
+}
