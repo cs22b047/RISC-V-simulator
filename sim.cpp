fc7037a5c03b94ebaf7e697de6f56e2a35f66dc6
cs22b047 <cs22b047@iittp.ac.in>
2024-02-23 04:19:22 +0530

    implemented PC

--- sim.cpp
+++ sim.cpp
@@ -3,12 +3,13 @@
 #include <vector>
 #include <sstream>
 #include <fstream>
-using namespace std;
+// using namespace std;
 class Core{
-    private:
+    public:
         long int regs[32];
         long int pc;
-        string instruction;
+        std::ifstream program_file;
+        std::string instruction;
         public:
         Core(){
             for(int i=0;i<32;i++){
@@ -16,11 +17,19 @@ class Core{
             }
             pc = 0;
         }
-        void execute(int* memory,string instruction){
-            this->instruction=instruction;
-            vector<string> parts;
-            string part;
-            istringstream iss(instruction);
+        bool execute(int* memory){
+            //assign instruction string to the line pc is pointing to
+            program_file.seekg(0);
+            for(int i=0;i<=pc;i++){
+                if(program_file.eof()) return false;
+                getline(program_file,instruction);
+            }
+            
+            std::cout<<instruction<<std::endl;
+            //splitting instruction
+            std::vector<std::string> parts;
+            std::string part;
+            std::istringstream iss(instruction);
             while(getline(iss,part,' ')){
                 parts.push_back(part);
             }
@@ -33,15 +42,10 @@ class Core{
             if(*it=="jal") execute_jal(it);
             if(*it=="bne") execute_bne(it);
             if(*it=="beq") execute_beq(it);
+            return true;
         }
-        int get_reg(vector<string>::iterator it){
-            int reg_id=0;
-            for(int i=1;i<(*it).length();i++){
-                reg_id = reg_id*10 + ((*it)[i]-48);
-            }
-            return reg_id;
-        }
-        void execute_addi(vector<string>::iterator it){
+        // execute functions
+        void execute_addi(std::vector<std::string>::iterator it){
             int x1 = get_reg(++it);
             int x2 = get_reg(++it);
             it++;
@@ -51,53 +55,74 @@ class Core{
                 x3 = x3*10 + ((*it)[i]-48);
             }
             regs[x1] = regs[x2] + x3; 
+            pc++;
         }
-        void execute_add(vector<string>::iterator it){
+        void execute_add(std::vector<std::string>::iterator it){
             int x1 = get_reg(++it);
             int x2 = get_reg(++it);
             int x3 = get_reg(++it);
             regs[x1] = regs[x2] + regs[x3];
+            pc++;
         }   
-        void execute_sub(vector<string>::iterator it){
+        void execute_sub(std::vector<std::string>::iterator it){
             int x1 = get_reg(++it);
             int x2 = get_reg(++it);
             int x3 = get_reg(++it);
             regs[x1] = regs[x2] - regs[x3];
+            pc++;
         }
-        void execute_lw(vector<string>::iterator it){
+        void execute_lw(std::vector<std::string>::iterator it){
 
-            cout<<"exe_addi";
+            std::cout<<"exe_addi";
         }
-        void execute_sw(vector<string>::iterator it){
-            cout<<"exe_addi";
+        void execute_sw(std::vector<std::string>::iterator it){
+            std::cout<<"exe_addi";
         }
-        void execute_jal(vector<string>::iterator it){
-            cout<<"exe_addi";
+        void execute_jal(std::vector<std::string>::iterator it){
+            std::cout<<"exe_addi";
         }
-        void execute_bne(vector<string>::iterator it){
-            cout<<"exe_addi";
+        void execute_bne(std::vector<std::string>::iterator it){
+            int x1 = get_reg(++it);
+            int x2 = get_reg(++it);
+            if(x1!=x2){
+                it++;
+                search_label(*it);
+            }else pc++;
         }
-        void execute_beq(vector<string>::iterator it){
-            cout<<"exe_addi";
+        void execute_beq(std::vector<std::string>::iterator it){
+            std::cout<<"exe_addi";
+        }
+        // util functions
+        int get_reg(std::vector<std::string>::iterator it){
+            int reg_id=0;
+            for(int i=1;i<(*it).length();i++){
+                reg_id = reg_id*10 + ((*it)[i]-48);
+            }
+            return reg_id;
+        }
+        void search_label(std::string label){
+            program_file.seekg(0);
+            std::string temp;
+            int i=0;
+            while(getline(program_file,temp)){
+                if(temp==(label+":")) break;
+                i++;
+            }
+            pc=i+1;
         }
         void printReg(){
             for(int i=0;i<32;i++){
-                cout<<"x"<<(i)<<":    "<<regs[i]<<endl;
+                std::cout<<"x"<<(i)<<":    "<<regs[i]<<std::endl;
             }
         }
 };
-
 class Processor{
     public:
         int* memory = (int*)malloc(4096);
         int clock = 0;
-        ifstream file_1,file_2;
-        string instruction_1,instruction_2;
         Core *cores[2] = {new Core(),new Core()};
         void run(){
-            while(getline(file_1,instruction_1)){
-                cores[0]->execute(memory,instruction_1);
-            }
+            while(cores[0]->execute(memory)){}
             cores[0]->printReg();
         }
 
@@ -105,8 +130,8 @@ class Processor{
 int main(){ 
     Processor sim;
 
-    sim.file_1.open("/home/tilak/Projects/Risc-v_sim/program1.txt");
-    sim.file_2.open("/home/tilak/Projects/Risc-v_sim/program2.txt");
+    sim.cores[0]->program_file.open("/home/tilak/Projects/Risc-v_sim/program1.txt");
+    sim.cores[1]->program_file.open("/home/tilak/Projects/Risc-v_sim/program2.txt");
     sim.run();
     return 0;
 }
