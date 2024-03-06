c40d7eb65ca64ba416c33c9c832a147a6eae0f54
cs22b047 <cs22b047@iittp.ac.in>
2024-03-07 03:35:10 +0530

    added cmake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tilak/Projects/Risc-v_sim
+# Build directory: /home/tilak/Projects/Risc-v_sim/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
