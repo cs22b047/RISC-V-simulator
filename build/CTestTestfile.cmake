7315dd4b423eb76216861038865f8c4888ae04ff
cs22b047 <cs22b047@iittp.ac.in>
2024-03-07 04:01:25 +0530

    added .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tilak/Projects/Risc-v_sim
-# Build directory: /home/tilak/Projects/Risc-v_sim/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
